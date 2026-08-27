cd550dc8ddd967ad42e687c7f62379ac391efd88
João Almeida <mr.johnplay0108@gmail.com>
2026-08-27 15:54:26 -0300

    Ajustado a responsividade das telas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,8 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login-aluno" element={<LoginAluno />} />
             <Route path="/cadastro-aluno" element={<CadastroAluno />} />
-            <Route path="/sorteio" element={<ProtectedRoute> <Sorteio /> </ProtectedRoute>} />
+            {/* <Route path="/sorteio" element={<ProtectedRoute> <Sorteio /> </ProtectedRoute>} /> */}
+            <Route path="/sorteio" element={ <Sorteio />} />
             <Route path="/eixo/:eixoId" element={<ProtectedRoute> <EixoGrupos /> </ProtectedRoute>} />
             <Route path="*" element={<NotFound />} />
           </Routes>
